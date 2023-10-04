cf4d40f96a0072afb0ec3db78b1b302883647439
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2023-10-04 09:26:45 +0200

    Update Records page design (#37)

--- apps/dashboard/src/app/app.config.ts
+++ apps/dashboard/src/app/app.config.ts
@@ -1,5 +1,7 @@
+import { registerLocaleData } from '@angular/common';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
-import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
+import localeUk from '@angular/common/locales/uk';
+import { APP_INITIALIZER, ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { AuthHttpInterceptor, AuthService, authHttpInterceptorFn, provideAuth0 } from '@auth0/auth0-angular';
@@ -13,6 +15,9 @@ import { environment } from '../../../../environments/environment';
 import { appRoutes } from './app.routes';
 import { ENVIRONMENT_CONFIG_TOKEN } from './core/configuration/tokens/environment-config.token';
 import { nzConfigFactory } from './core/nz-config/nz-config.factory';
+
+// for the future
+registerLocaleData(localeUk, 'uk');
 
 function initAppFactory(authService: AuthService): () => Observable<any> {
   return () => {
@@ -29,6 +34,7 @@ function initAppFactory(authService: AuthService): () => Observable<any> {
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: LOCALE_ID, useValue: 'en-US' },
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     provideHttpClient(withInterceptors([authHttpInterceptorFn])),
     provideAuth0({
