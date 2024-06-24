5e2d868160a5a59e68f988a7651b2fee05a3a12b
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2024-06-24 16:57:14 +0200

    Move planned payment helper functions to separate service

--- apps/dashboard/src/app/shared/components/planned-payments-calendar/planned-payments-calendar.component.ts
+++ apps/dashboard/src/app/shared/components/planned-payments-calendar/planned-payments-calendar.component.ts
@@ -1,12 +1,12 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
-import { DateTime } from 'luxon';
+import { ChangeDetectionStrategy, Component, computed, inject, input } from '@angular/core';
 
 import { NzCalendarModule } from 'ng-zorro-antd/calendar';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 
-import { PeriodEnum, RecordTypeEnum } from '@financial-project/common';
+import { RecordTypeEnum } from '@financial-project/common';
 
 import { PlannedPaymentModel } from '../../../api/planned-payments/models/planned-payment.model';
+import { PlannedPaymentsHelpersService } from '../../../dashboard/services/planned-payments-helpers.service';
 import { AmountFormatPipe } from '../../pipes/amount-format/amount-format.pipe';
 import { CurrencyFormatPipe } from '../../pipes/currency-format/currency-format.pipe';
 import { ToDefaultCurrencyPipe } from '../../pipes/to-default-currency/to-default-currency.pipe';
@@ -24,110 +24,17 @@ export class PlannedPaymentsCalendarComponent {
 
   readonly RecordTypeEnum = RecordTypeEnum;
 
+  private plannedPaymentsHelpersService = inject(PlannedPaymentsHelpersService);
+
   private data = computed(() => {
     const plannedPayments = this.plannedPayments();
 
-    const processedData = new Map<string, PlannedPaymentModel[]>();
-
-    for (const plannedPayment of plannedPayments) {
-      switch (plannedPayment.period) {
-        case PeriodEnum.OneTime: {
-          this.setPlannedPaymentToProcessedData(
-            processedData,
-            this.getOneTimePaymentKey(plannedPayment.dateOfYear!),
-            plannedPayment,
-          );
-          break;
-        }
-
-        case PeriodEnum.Yearly: {
-          this.setPlannedPaymentToProcessedData(
-            processedData,
-            this.getYearlyPaymentKey(plannedPayment.dateOfYear!),
-            plannedPayment,
-          );
-          break;
-        }
-
-        case PeriodEnum.Monthly: {
-          this.setPlannedPaymentToProcessedData(
-            processedData,
-            this.getMonthlyPaymentKey(plannedPayment.dayOfMonth!),
-            plannedPayment,
-          );
-          break;
-        }
-
-        case PeriodEnum.Weekly: {
-          this.setPlannedPaymentToProcessedData(
-            processedData,
-            this.getWeeklyPaymentKey(plannedPayment.dayOfWeek!),
-            plannedPayment,
-          );
-          break;
-        }
-      }
-    }
-
-    return processedData;
+    return this.plannedPaymentsHelpersService.processPlannedPayments(plannedPayments);
   });
-
-  setPlannedPaymentToProcessedData(
-    processedData: Map<string, PlannedPaymentModel[]>,
-    key: string,
-    plannedPayment: PlannedPaymentModel,
-  ): void {
-    if (processedData.has(key)) {
-      processedData.get(key)?.push(plannedPayment);
-    } else {
-      processedData.set(key, [plannedPayment]);
-    }
-  }
 
   getPlannedPaymentsForDate(date: Date): PlannedPaymentModel[] {
     const plannedPaymentsMap = this.data();
 
-    const plannedPayments: PlannedPaymentModel[] = [];
-
-    const dt = DateTime.fromJSDate(date);
-
-    const monthly = plannedPaymentsMap.get(this.getMonthlyPaymentKey(dt.get('day')));
-    const weekly = plannedPaymentsMap.get(this.getWeeklyPaymentKey(dt.get('weekday')));
-    const yearly = plannedPaymentsMap.get(this.getYearlyPaymentKey(date));
-    const oneTime = plannedPaymentsMap.get(this.getOneTimePaymentKey(date));
-
-    if (monthly?.length) {
-      plannedPayments.push(...monthly);
-    }
-
-    if (weekly?.length) {
-      plannedPayments.push(...weekly);
-    }
-
-    if (yearly?.length) {
-      plannedPayments.push(...yearly);
-    }
-
-    if (oneTime?.length) {
-      plannedPayments.push(...oneTime);
-    }
-
-    return plannedPayments;
-  }
-
-  getMonthlyPaymentKey(dayOfMonth: number): string {
-    return `${PeriodEnum.Monthly}.${dayOfMonth}`;
-  }
-
-  getWeeklyPaymentKey(dayOfWeek: number): string {
-    return `${PeriodEnum.Weekly}.${dayOfWeek}`;
-  }
-
-  getYearlyPaymentKey(dateOfYear: Date): string {
-    return `${PeriodEnum.Yearly}.${DateTime.fromJSDate(dateOfYear).toFormat('yyyy-MM-dd')}`;
-  }
-
-  getOneTimePaymentKey(dateOfYear: Date): string {
-    return `${PeriodEnum.OneTime}.${DateTime.fromJSDate(dateOfYear).toFormat('yyyy-MM-dd')}`;
+    return this.plannedPaymentsHelpersService.getPlannedPaymentsForDate(plannedPaymentsMap, date);
   }
 }
