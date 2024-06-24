65b5d7ed48ca84a03ea0be35fc1e728d8ce52898
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2024-06-24 21:05:30 +0200

    Change budget snapshots API

--- apps/api/src/app/budget-snapshots/budget-snapshots.module.ts
+++ apps/api/src/app/budget-snapshots/budget-snapshots.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthenticationModule } from '../authentication/authentication.module';
 import { BudgetEntity } from '../budgets/entities/budget.entity';
+import { CategoriesModule } from '../categories/categories.module';
 import { BudgetSnapshotsController } from './budget-snapshots.controller';
 import { BudgetSnapshotsService } from './budget-snapshots.service';
 import { BudgetSnapshotEntity } from './entities/budget-snapshot.entity';
@@ -10,7 +11,7 @@ import { BudgetSnapshotEntity } from './entities/budget-snapshot.entity';
 @Module({
   controllers: [BudgetSnapshotsController],
   providers: [BudgetSnapshotsService],
-  imports: [TypeOrmModule.forFeature([BudgetSnapshotEntity, BudgetEntity]), AuthenticationModule],
+  imports: [TypeOrmModule.forFeature([BudgetSnapshotEntity, BudgetEntity]), CategoriesModule, AuthenticationModule],
   exports: [BudgetSnapshotsService],
 })
 export class BudgetSnapshotsModule {}
