86129ac2266ef085e91760091804812ad1b82e06
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2024-06-24 17:10:59 +0200

    Add dynamic titles

--- apps/dashboard/src/app/dashboard/dashboard.routes.ts
+++ apps/dashboard/src/app/dashboard/dashboard.routes.ts
@@ -16,26 +16,31 @@ export const dashboardRoutes: Route[] = [
       },
       {
         path: RoutePathEnum.Analytics,
+        title: 'Budget - Analytics',
         loadComponent: () =>
           import('./analytics-container/analytics-container.component').then((mod) => mod.AnalyticsContainerComponent),
       },
       {
         path: RoutePathEnum.Providers,
+        title: 'Budget - Banks',
         loadComponent: () =>
           import('./providers-container/providers-container.component').then((mod) => mod.ProvidersContainerComponent),
       },
       {
         path: RoutePathEnum.Accounts,
+        title: 'Budget - Accounts',
         loadComponent: () =>
           import('./accounts-container/accounts-container.component').then((mod) => mod.AccountsContainerComponent),
       },
       {
         path: RoutePathEnum.Records,
+        title: 'Budget - Records',
         loadComponent: () =>
           import('./records-container/records-container.component').then((mod) => mod.RecordsContainerComponent),
       },
       {
         path: RoutePathEnum.Categories,
+        title: 'Budget - Categories',
         loadComponent: () =>
           import('./categories-container/categories-container.component').then(
             (mod) => mod.CategoriesContainerComponent,
@@ -43,11 +48,13 @@ export const dashboardRoutes: Route[] = [
       },
       {
         path: RoutePathEnum.Budgets,
+        title: 'Budget - Budgets',
         loadComponent: () =>
           import('./budgets-container/budgets-container.component').then((mod) => mod.BudgetsContainerComponent),
       },
       {
         path: RoutePathEnum.Settings,
+        title: 'Budget - Settings',
         loadComponent: () =>
           import('./settings-container/settings-container.component').then((mod) => mod.SettingsContainerComponent),
       },
